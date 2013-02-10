265962057e3bf8f91374bb1de1fcf1d1a082ec9a
nalabjp <nalabjp@gmail.com>
2013-02-11 01:24:17 +0900

    Create event_site

---
+++ lib/event4r/event_site.rb
@@ -0,0 +1,28 @@
+# encoding: utf-8
+require_relative "http_client"
+
+module Event4r
+  class EventSite
+    def initialize
+      raise NotImplementedError, "need to call create_client"
+    end
+
+    def search_event(params = {})
+      raise NotImplementedError, "need to call search_event_impl"
+    end
+
+    def create_client(host)
+      @client = HTTPClient.new(host)
+    end
+
+    def search_event_impl(path, params = {})
+      return @client.request_json(path, make_query(params))
+    end
+
+    def make_query(params)
+      params[:format] = "json"
+      params.map { |k, v| "#{k}=#{v}" }.join("&")
+    end
+    protected :create_client, :make_query, :search_event_impl
+  end
+end
---
+++ spec/event4r/event_site_spec.rb
@@ -0,0 +1,11 @@
+# coding: utf-8
+require_relative '../spec_helper'
+require_relative '../../lib/event4r/event_site'
+
+describe Event4r::EventSite do
+  context "when initialize" do
+    it "raise NotImplementedError" do
+      lambda{Event4r::EventSite.new}.should raise_error(NotImplementedError)
+    end
+  end
+end
